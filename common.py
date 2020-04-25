b497c824acf2c627acdc8331e4edafa5d95c6b48
Mitchell Kember <mk12360@gmail.com>
2020-04-25 02:28:38 -0400

    Refactor python scripts with Puzzle class

--- common.py
+++ common.py
@@ -1,14 +1,29 @@
+from __future__ import annotations
+
+import argparse
 import contextlib
 import re
 import sys
+from abc import ABC, abstractmethod
+from typing import Any, Collection, Dict, Iterator, Set, TextIO, Tuple, Type, Union
 
 
-def parse_instance(stream):
+# Either an integer (for the Int set) or an atom like Foo or Foo$0.
+Atom = Union[int, str, Tuple[str, int]]
+
+# An n-ary relation represented as a nested dictionary with sets as leaves.
+Relation = Union[Set[Atom], Dict[str, Any]]
+
+# A group of named sets/relations.
+Instance = Dict[str, Relation]
+
+
+def parse_instance(stream: TextIO) -> Instance:
     """Parse output from the Txt tab in an Alloy instance."""
     instance = {}
     for line in stream:
         line = line.strip()
-        m = re.fullmatch(r'([a-zA-Z0-9$/<:]+)={(.*)}', line)
+        m = re.fullmatch(r"([a-zA-Z0-9$/<:]+)={(.*)}", line)
         if not m:
             continue
         name = m.group(1)
@@ -20,9 +35,9 @@ def parse_instance(stream):
     return instance
 
 
-def parse_atom(string):
+def parse_atom(string: str) -> Atom:
     """Parse an atom from a string representation."""
-    if re.fullmatch(r'-?[0-9]+', string):
+    if re.fullmatch(r"-?[0-9]+", string):
         return int(string)
     if "$" not in string:
         return string
@@ -30,7 +45,7 @@ def parse_atom(string):
     return name, int(number)
 
 
-def build_relation(vals):
+def build_relation(vals: Collection[str]) -> Relation:
     """Build a nested dict/set from a list of tuples in a relation."""
     root = {}
     for v in vals:
@@ -48,7 +63,7 @@ def build_relation(vals):
     return setify(root)
 
 
-def the(node):
+def the(node: Relation) -> Atom:
     """Return the element of a singleton set"""
     assert len(node) == 1
     return next(iter(node))
@@ -67,3 +82,145 @@ def alternate_screen():
 def clear_screen():
     """Clear the terminal screen."""
     print("\x1b[2J\x1b[H", end="")
+
+
+def default_main(puzzle_class: Type[Puzzle]):
+    """A default main function for puzzle scripts."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interactive", action="store_true")
+    parser.add_argument("file", help="file containing Alloy instance txt")
+    args = parser.parse_args()
+    with open(args.file) as f:
+        puzzle = puzzle_class(parse_instance(f))
+    if args.interactive:
+        puzzle.interactive()
+    else:
+        puzzle.dump()
+
+
+# Domain representation of a puzzle state.
+State = Any
+
+
+class Puzzle(ABC):
+
+    """Generic puzzle.
+
+    It assumes there is a signature called State.
+
+    This class is responsible for parsing an Alloy instance of the puzzle
+    solution and for printing it in various formats.
+    """
+
+    def __init__(self, instance: Instance):
+        self.instance = instance
+        self.setup()
+
+    def setup(self):
+        ...
+
+    def states(self) -> Iterator[Tuple[int, State]]:
+        for s in sorted(self.instance["this/State"]):
+            index = s[1] + 1  # convert to 1-based
+            yield index, self.build_state(s)
+
+    @abstractmethod
+    def build_state(self, s: Atom) -> State:
+        ...
+
+    def dump(self):
+        for index, state in self.states():
+            print(f"{'-' * 80}\nState {index}\n")
+            self.print_state(state)
+            print()
+
+    def interactive(self):
+        with alternate_screen():
+            for index, state in self.states():
+                print(f"State {index}\n")
+                self.print_state(state)
+                input()
+                clear_screen()
+
+    @abstractmethod
+    def print_state(self, state: State):
+        ...
+
+
+class CoordPuzzle(Puzzle):
+
+    """A puzzle based on coordinates.
+
+    It assumes the following signatures/relations:
+
+        sig Coord {
+            x, y: Int
+        }
+
+        sig State {
+            -- (1)
+            NAME: lone OBJECT -> one Coord
+            -- (2)
+            NAME: Coord -> one OBJECT
+        }
+
+    The NAME is specified by object_to_cood for (1), or coord_to_object for (2).
+    The OBJECT can be named anything.
+    """
+
+    # Inclusive ranges.
+    x_range: Tuple[int, int]
+    y_range: Tuple[int, int]
+
+    # Only one of these should be set.
+    object_to_coord: str = ""
+    coord_to_object: str = ""
+
+    # Optional.
+    static_to_coord: str = ""
+
+    def setup(self):
+        coord = self.instance["this/Coord"]
+        x = self.instance["this/Coord<:x"]
+        y = self.instance["this/Coord<:y"]
+        self.coords = {c: (the(x[c]), the(y[c])) for c in coord}
+        self.all_xy = set(self.coords.values())
+        self.static = {}
+        if self.static_to_coord:
+            obj_coord = self.instance[f"this/{self.static_to_coord}"]
+            if obj_coord:
+                self.static = {
+                    self.coords[the(coord)]: obj for obj, coord in obj_coord.items()
+                }
+
+    def build_state(self, s: Atom) -> State:
+        if self.object_to_coord:
+            obj_coord = self.instance[f"this/State<:{self.object_to_coord}"][s]
+            state = {self.coords[the(coord)]: obj for obj, coord in obj_coord.items()}
+        elif self.coord_to_object:
+            coord_obj = self.instance[f"this/State<:{self.coord_to_object}"][s]
+            state = {self.coords[coord]: the(obj) for coord, obj in coord_obj.items()}
+        else:
+            assert False
+        return {**self.static, **state}
+
+    def print_state(self, state: State):
+        p = lambda s: print(s, end="")
+        ys = self.y_range[0], self.y_range[1] + 1
+        xs = self.x_range[0], self.x_range[1] + 1
+        for y in range(*ys):
+            for x in range(*xs):
+                if (x, y) in state:
+                    obj = state[(x, y)]
+                    p(self.object_char(obj))
+                elif (x, y) in self.all_xy:
+                    p(".")
+                else:
+                    p(" ")
+                if x != 2:
+                    p(" ")
+            print()
+
+    @abstractmethod
+    def object_char(self, obj: Any) -> str:
+        ...
--- sacred-grove.py
+++ sacred-grove.py
@@ -1,79 +1,23 @@
 #!/usr/bin/env python3
 
-import argparse
-
-from common import alternate_screen, clear_screen, the, parse_instance
+from common import CoordPuzzle, default_main
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interactive", action="store_true")
-    parser.add_argument("file", help="file containing Alloy instance txt")
-    args = parser.parse_args()
-    with open(args.file) as f:
-        puzzle = Puzzle(parse_instance(f))
-    if args.interactive:
-        puzzle.interactive()
-    else:
-        puzzle.dump()
+class SacredGrove(CoordPuzzle):
 
+    x_range = -2, 2
+    y_range = -3, 2
 
-class Puzzle:
+    object_to_coord = "pos"
 
-    def __init__(self, instance):
-        self.instance = instance
-        x_rel = instance["this/Coord<:x"]
-        y_rel = instance["this/Coord<:y"]
-        self.coords = {
-            c: (the(x_rel[c]), the(y_rel[c]))
-            for c in instance["this/Coord"]
-        }
-        self.all_coords = set(self.coords.values())
-
-    def states(self):
-        for s in sorted(self.instance["this/State"]):
-            positions = self.instance["this/State<:pos"][s]
-            index = s[1] + 1  # convert to 1-based
-            state = {
-                self.coords[the(pos)]: obj
-                for obj, pos in positions.items()
-            }
-            yield index, state
-
-    def dump(self):
-        for index, state in self.states():
-            print(f"{'-' * 80}\nState {index}\n")
-            self.print_state(state)
-            print()
-
-    def interactive(self):
-        with alternate_screen():
-            for index, state in self.states():
-                print(f"State {index}\n")
-                self.print_state(state)
-                input()
-                clear_screen()
-
-    def print_state(self, state):
-        p = lambda s: print(s, end="")
-        for y in range(-3, 3):
-            for x in range(-2, 3):
-                if (x, y) in state:
-                    obj = state[(x, y)]
-                    if obj == ("Link", 0):
-                        p("L")
-                    elif obj == ("StatueA", 0):
-                        p("A")
-                    elif obj == ("StatueB", 0):
-                        p("B")
-                elif (x, y) in self.all_coords:
-                    p(".")
-                else:
-                    p(" ")
-                if x != 2:
-                    p(" ")
-            print()
+    def object_char(self, obj):
+        if obj == ("Link", 0):
+            return "L"
+        if obj == ("StatueA", 0):
+            return "A"
+        if obj == ("StatueB", 0):
+            return "B"
 
 
 if __name__ == "__main__":
-    main()
+    default_main(SacredGrove)
